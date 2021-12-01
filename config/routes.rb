b0a9366335602320aa72d7eef89f63c42b747bf4
myhearisranch <myhearisranch@gmail.com>
2021-12-01 08:08:52 +0000

    routeの順番を入れ替えた, introductionのバリデーションを変えた, attachment :profile_imageにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   get 'home/about', to: "homes#about"
-  resources :users
   devise_for :users
+  resources :users
   resources :books
 
 end
